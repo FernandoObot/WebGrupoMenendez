aff11832d33bbf37345013645f403c4be95a28db
Luis Fernando Gutierrez Arellano <fernando@obotGID.ipv6.infinitum.net.mx>
2026-09-17 22:43:31 -0600

    feat(Inicio): Agregar la sección de Contactanos

--- GrupoMenendez/src/app/app.ts
+++ GrupoMenendez/src/app/app.ts
@@ -7,9 +7,10 @@ import { SobreNosotros } from './sobre-nosotros/sobre-nosotros';
 import { Servicios } from './servicios/servicios';
 import { ProyectosDestacados } from './proyectos-destacados/proyectos-destacados';
 import { NuestroProceso } from './nuestro-proceso/nuestro-proceso';
+import { CtaContacto } from './cta-contacto/cta-contacto';
 
 @Component({
-  imports: [RouterOutlet, Navbar, Footer, Presentacion, SobreNosotros, Servicios, ProyectosDestacados, NuestroProceso],
+  imports: [RouterOutlet, Navbar, Footer, Presentacion, SobreNosotros, Servicios, ProyectosDestacados, NuestroProceso, CtaContacto],
   selector: 'app-root',
   styleUrl: './app.css',
   templateUrl: './app.html',
---
+++ GrupoMenendez/src/app/cta-contacto/cta-contacto.ts
@@ -0,0 +1,9 @@
+import { Component } from '@angular/core';
+
+@Component({
+  imports: [],
+  selector: 'app-cta-contacto',
+  styleUrl: './cta-contacto.css',
+  templateUrl: './cta-contacto.html',
+})
+export class CtaContacto {}
