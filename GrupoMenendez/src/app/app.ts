c00f0dc192ffeac214b515439b81a99ddecdf0d6
Luis Fernando Gutierrez Arellano <fernando@obotGID.ipv6.infinitum.net.mx>
2026-09-17 21:30:49 -0600

    feat(Inicio): Agregar presentación de la empresa

--- GrupoMenendez/src/app/app.ts
+++ GrupoMenendez/src/app/app.ts
@@ -2,9 +2,10 @@ import { Component, signal } from '@angular/core';
 import { RouterOutlet } from '@angular/router';
 import { Navbar } from './navbar/navbar';
 import { Footer } from './footer/footer';
+import { Presentacion } from './presentacion/presentacion';
 
 @Component({
-  imports: [RouterOutlet, Navbar, Footer],
+  imports: [RouterOutlet, Navbar, Footer, Presentacion],
   selector: 'app-root',
   styleUrl: './app.css',
   templateUrl: './app.html',
---
+++ GrupoMenendez/src/app/presentacion/presentacion.ts
@@ -0,0 +1,9 @@
+import { Component } from '@angular/core';
+
+@Component({
+  imports: [],
+  selector: 'app-presentacion',
+  styleUrl: './presentacion.css',
+  templateUrl: './presentacion.html',
+})
+export class Presentacion {}
